b8d7c3894008c671628762804cc3b99c7ab1904d
James Nettik <james@atendesigngroup.com>
2018-01-26 08:19:27 -0700

    Created config autoloader.

---
+++ config/README.md
@@ -0,0 +1,4 @@
+# Tooling Configuration
+
+Config files for specific taskes can be loaded via `require('./config')` with
+each file's name being a key to reference data from.
--- config/index.js
+++ config/index.js
@@ -1,2 +1,18 @@
+/**
+ * @file
+ * Include and export all config files into their own modules.
+ */
 
-module.exports = {};
+const fs = require('fs');
+const path = require('path');
+
+fs.readdirSync(__dirname).forEach((file) => {
+  // Exclude current file.
+  if (file === path.basename(__filename)) { return; }
+  // Exclude non-JS files.
+  if (path.extname(file) !== '.js') { return; }
+
+  // Export modules named after each config file.
+  // eslint-disable-next-line import/no-dynamic-require
+  module.exports[path.basename(file, '.js')] = require(path.join(__dirname, file));
+});
